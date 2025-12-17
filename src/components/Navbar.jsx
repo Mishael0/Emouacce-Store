f0e6b0ed0fea102d13eef57dadd73e5273eb6cf5
UdoEsinna Mishael <udoesinna@gmail.com>
2025-12-17 16:57:17 +0100

    feat: added search input and styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,10 @@
 
 function Navbar() {
   return (
-    <nav className="bg-white shadow-md ">
+    <nav className="sticky top-0 z-50 bg-white shadow-md ">
         <div className="max-w-7xl mx-auto p-4 flex justify-between items-center">
              <div>
-            <h1 className="text-[#C2B280] text-base sm:text-xl md:text-2xl font-bold">EMOUACCE STORE</h1>
+            <h1 className="text-[#918252] text-2xl sm:text-3xl md:text-4xl font-extrabold tracking-[0.07em]">EMOUACCE</h1>
         </div>
 
         <div className="flex gap-2 md:gap-4">
@@ -14,7 +14,11 @@ function Navbar() {
                <button>Cart</button>
         </div>
         </div>
-       
+       <div className="flex justify-center w-full py-4">
+            <input type="text" 
+            placeholder="Search Your Products"
+            className="w-full max-w-md px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus-ring-2 focus:ring-[#F5F5DC] bg-white text-gray-600" />
+        </div>
     </nav>
   )
 }
