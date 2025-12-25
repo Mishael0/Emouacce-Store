12b853084fcf0a29b6ade016b26e3f1cb899b304
UdoEsinna Mishael <udoesinna@gmail.com>
2025-12-25 07:45:50 +0100

    added svg code for my logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'spin-slow': 'spin 15s  linear infinite',
+        'pulse-soft': 'pulse-glow3s ease-in-out infinite',
+      },
+      keyframes:{
+        'pulse-glow': {
+         '0%, 100%': {opacity: '1', filter: 'drop-shadow(0 0 2px rgba(255,255,255,0.8))' },
+         '50%': {opacity: '0.7', filter: 'drop-shadow(0 8px rgba(255,255,255,1))' },
+        },
+      },
+    },
   },
   plugins: [],
 }
